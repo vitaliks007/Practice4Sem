08f283be4b8b822b0c77d1b948ca1dbf73e92ecb
Vitaliy <svitalik2002@gmail.com>
2022-03-05 18:53:07 +0300

    Threads fix

--- src/com/Vitaliy/task3/Main.java
+++ src/com/Vitaliy/task3/Main.java
@@ -2,57 +2,57 @@ package com.Vitaliy.task3;
 
 public class Main {
     static volatile ListSafe<Integer> list = new ListSafe<>();
-    static void increment(ListSafe<Integer> list) {
-        list.set(0, (int)list.get(0) + 1);
-    }
-
+//    static void increment(ListSafe<Integer> list) {
+//        list.set(0, (int)list.get(0) + 1);
+//    }
+//
     static volatile SetSafe<Integer> set = new SetSafe<>();
-    static void increment(SetSafe<Integer> set) {
-        int o = -100;
-
-        for (Object elem : set) {
-            o = (int)elem + 1;
-        }
-        set.clear();
-        set.add(o);
-    }
+//    static void increment(SetSafe<Integer> set) {
+//        int o = -100;
+//
+//        for (Object elem : set) {
+//            o = (int)elem + 1;
+//        }
+//        set.clear();
+//        set.add(o);
+//    }
 
     static void listTest() throws InterruptedException {
-        list.add(0);
-
         Thread one = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                increment(list);
+                list.add(i);
             }
         });
         Thread two = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                increment(list);
+                list.add(i + 100);
             }
         });
         one.start();
         two.start();
         Thread.sleep(1000);
+        one.interrupt();
+        two.interrupt();
 
         System.out.println(list);
     }
 
     static void setTest() throws InterruptedException {
-        set.add(0);
-
         Thread one = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                increment(set);
+                set.add(i);
             }
         });
         Thread two = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                increment(set);
+                set.add(i + 100);
             }
         });
         one.start();
         two.start();
         Thread.sleep(1000);
+        one.interrupt();
+        two.interrupt();
 
         System.out.println(set);
     }
